77b753dc00c3db12cc9dfa0eb8ec9745e602f422
Alex Hampton <ahampton5158@gmail.com>
2020-04-01 22:10:47 +0900

    Update board to accept input and render simple changes

--- Ruby/minesweeper/board.rb
+++ Ruby/minesweeper/board.rb
@@ -1,4 +1,5 @@
 require_relative 'tile'
+require 'byebug'
 
 class Board
     attr_reader :grid
@@ -15,7 +16,6 @@ class Board
         self.grid.each.with_index do |row, idx|
             print "#{idx} "
             row.each.with_index do |tile, col_idx|
-                # sym = tile.revealed ? "_" : "X"
                 print "#{tile.which_symbol} "
             end
             puts
@@ -25,16 +25,64 @@ class Board
     end
 
     def run
-        if self.lose?
-            puts "You lose"
-            return
-        end
         until self.win?
+            if self.lose?
+                puts "You lose."
+                return
+            end
             self.render
-            puts "Please select a space (Ex: 0,0)."
+
+            pos = false
+            until self.valid_pos?(pos)
+                pos = self.get_pos
+            end
+            val = false
+            until self.valid_val?(val)
+                val = self.get_val
+            end
+            self.update(pos[0], pos[1], val)
         end
         puts "You win!"
         return
+    end
+
+    def update(pos_1, pos_2, val)
+        tile = grid[pos_1][pos_2]
+        if val == "F"
+            tile.flag
+        else
+            tile.reveal
+        end
+    end
+
+    def valid_pos?(pos)
+        pos.is_a?(Array) && 
+        pos.length == 2 && 
+        pos.all? { |num| (0..8).include?(num) }
+    end
+
+    def valid_val?(val)
+        val == "F" || val == "R"
+    end
+
+    def get_pos
+        puts "Please select a position (Ex. '0,0')"
+        pos = gets.chomp
+        parse_pos(pos)
+    end
+
+    def get_val
+        puts "Press 'R' to reveal, 'F' to flag."
+        val = gets.chomp.upcase
+    end
+
+    def parse_pos(pos)
+        begin
+            pos.split(',').map { |num| Integer(num) }
+        rescue => exception
+            puts "Please input numbers only, separated by a comma."
+        end
+        
     end
 
     def win?
