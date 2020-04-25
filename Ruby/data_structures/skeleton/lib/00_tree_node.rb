b473c5791e9fe317642b3f581dd16edecb97e75d
Alex Hampton <ahampton5158@gmail.com>
2020-04-25 14:27:41 +0900

    Create Tree node class with DFS and BFS search capabilities

--- Ruby/data_structures/skeleton/lib/00_tree_node.rb
+++ Ruby/data_structures/skeleton/lib/00_tree_node.rb
@@ -1,5 +1,7 @@
 require 'byebug'
 class PolyTreeNode
+
+    attr_reader :value
     def initialize(value)
         @value = value
         @parent = nil
@@ -37,32 +39,47 @@ class PolyTreeNode
         @value
     end
 
+    # Depth First Search
     def dfs(target_value)
-        print self if @value == target_value
-        return self if @value == target_value
+        # puts self.value if self.value == target_value        
+        return self if self.value == target_value
 
-        @children.each do |child|
-            child.dfs(target_value)
+        self.children.each do |child|
+            search_result = child.dfs(target_value)
+            return search_result unless search_result.nil?
         end
+        # When it gets to the end of the leaf, it returns that node in an array...
+        nil
+    end
 
-        puts self.value
+    #Breadth First Search
+    def bfs(target_value)
+        debugger
+        queue = []
+        queue << self
+        until queue.empty?            
+            current_node = queue.shift
+            return current_node if current_node.value == target_value
+            queue += current_node.children     
+        end  
+        nil
     end
 
 end
 
 
-# n1 = PolyTreeNode.new(1)
-# n2 = PolyTreeNode.new(2)
-# n3 = PolyTreeNode.new(3)
-# n4 = PolyTreeNode.new(4)
-# n5 = PolyTreeNode.new(5)
-# n6 = PolyTreeNode.new(6)
+n1 = PolyTreeNode.new(1)
+n2 = PolyTreeNode.new(2)
+n3 = PolyTreeNode.new(3)
+n4 = PolyTreeNode.new(4)
+n5 = PolyTreeNode.new(5)
+n6 = PolyTreeNode.new(6)
 
-# n1.add_child(n2)
-# n1.add_child(n3)
-# n2.add_child(n4)
-# n2.add_child(n5)
-# n3.add_child(n6)
+n1.add_child(n2)
+n1.add_child(n3)
+n2.add_child(n4)
+n2.add_child(n5)
+n3.add_child(n6)
 
 # #       n1
 # #      /  \
@@ -71,4 +88,4 @@ end
 # # n4  n5 n6 
 # #
 
-# n1.dfs(3)
+p n1.bfs(3)
