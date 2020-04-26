dea05f33cad1273f6d38ca98d2cf82d1d7b9c623
Alex Hampton <ahampton5158@gmail.com>
2020-04-26 11:39:32 +0900

    Revise valid_moves

--- Ruby/data_structures/knights_travails/knight_path_finder.rb
+++ Ruby/data_structures/knights_travails/knight_path_finder.rb
@@ -4,11 +4,9 @@ class KnightPathFinder
     attr_reader :root_node
 
     def initialize(pos)
-        @root_node = PolyTreeNode.new(pos)
         @considered_positions = [pos]
-
-        def build_move_tree
-            start_pos = @root_node
+        def build_move_tree(pos)
+            start_pos = PolyTreeNode.new(pos)
             queue = [start_pos]
 
             until queue.empty?
@@ -21,58 +19,34 @@ class KnightPathFinder
             end
             start_pos
         end
+        @root_node = build_move_tree(pos)
+
 
     end
 
     # Will make an array of all possible moves from the pos. I think this should be a class method because regardless of the instance, the valid moves from any given position will always be the same from that position.
     def self.valid_moves(pos)
+        current_x, current_y = pos
         valid_moves = []
-        x,y = pos[0],pos[1]
 
-        # Get upper positions
-        if y >= 2
-            if x >= 1
-                upper_left = [x-1, y-2]
-                valid_moves << upper_left
-            end
-            if  x < 7
-                upper_right = [x+1, y-2]
-                valid_moves << upper_right
-            end
-        end
-        # Get lower positions
-        if y <= 5
-            if x >= 1
-                lower_left = [x-1, y+2]
-                valid_moves << lower_left
-            end
-            if x < 7
-                lower_right = [x+1, y+2]
-                valid_moves << lower_right
+        moves = [
+            [-1, -2],
+            [ 1, -2],
+            [-1,  2],
+            [ 1,  2],
+            [-2, -1],
+            [-2,  1],
+            [ 2, -1],
+            [ 2,  1]
+        ]
+
+        moves.each do |(x, y)|
+            new_pos = [current_x + x, current_y + y]
+            if new_pos.all? { |num| num.between?(0,7) }
+                valid_moves << new_pos
             end
         end
-        # Get left positions
-        if x >= 2
-            if y >= 1
-                left_upper = [x-2, y-1]
-                valid_moves << left_upper
-            end
-            if y < 7
-                left_lower = [x-2, y+1]
-                valid_moves << left_lower
-            end
-        end
-        #Get right positions
-        if x <= 5
-            if y >=1
-                right_upper = [x+2, y-1]
-                valid_moves << right_upper
-            end
-            if y < 7
-                right_lower = [x+2, y+1]
-                valid_moves << right_lower
-            end
-        end
+
         valid_moves
     end
 
@@ -88,25 +62,26 @@ end
 
 k = KnightPathFinder.new([0,0])
 
-# parent = k.build_move_tree
+p k.root_node
 
-# cren = []
-# k.build_move_tree.children.each { |child| cren << child}
 
-# gchildren = []
-# cren.each { |child| child.children.each { |gchild| gchildren << gchild }}
+cren = []
+k.root_node.children.each { |child| cren << child}
 
-# ggchildren = []
-# gchildren.each {|gchild| gchild.children.each { |ggchild| ggchildren << ggchild }}
+gchildren = []
+cren.each { |child| child.children.each { |gchild| gchildren << gchild }}
 
-# gggchildren = []
-# ggchildren.each { |ggchild| ggchild.children.each { |gggchild| gggchildren << gggchild }}
+ggchildren = []
+gchildren.each {|gchild| gchild.children.each { |ggchild| ggchildren << ggchild }}
 
-# ggggchildren = []
-# gggchildren.each { |gggchild| gggchild.children.each { |ggggchild| ggggchildren << ggggchild }}
-# ggggchildren.each { |ggggc| p ggggc.value }
-# puts
-# puts
-# gggggchildren = []
-# ggggchildren.each { |ggggchild| ggggchild.children.each { |gggggchild| gggggchildren << gggggchild }}
-# gggggchildren.each { |gggggc| p gggggc.value }
+gggchildren = []
+ggchildren.each { |ggchild| ggchild.children.each { |gggchild| gggchildren << gggchild }}
+
+ggggchildren = []
+gggchildren.each { |gggchild| gggchild.children.each { |ggggchild| ggggchildren << ggggchild }}
+ggggchildren.each { |ggggc| p ggggc.value }
+puts
+puts
+gggggchildren = []
+ggggchildren.each { |ggggchild| ggggchild.children.each { |gggggchild| gggggchildren << gggggchild }}
+gggggchildren.each { |gggggc| p gggggc.value }
