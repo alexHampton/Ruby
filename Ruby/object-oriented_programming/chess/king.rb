81062244b8a7f43c28762e47b13aa76531f203e8
Alex Hampton <ahampton5158@gmail.com>
2020-05-02 18:25:44 +0900

    Add move_difs methods to stepable classes

--- Ruby/object-oriented_programming/chess/king.rb
+++ Ruby/object-oriented_programming/chess/king.rb
@@ -1,11 +1,28 @@
 require_relative 'piece'
+require_relative 'module_stepable'
+require 'byebug'
 
 class King < Piece
+    include Stepable
     def initialize(color, board, pos)
         super
     end
 
     def symbol
         :K
+    end
+
+    
+    # return array of all positions piece could move to without obstacles
+    def move_difs
+        x,y = self.pos[0], pos[1]
+        top_side, bottom_side = x-1, x+1
+        left_side, right_side = y-1, y+1
+        moves = []
+        moves.push([top_side, left_side], [top_side, y], [top_side, right_side])
+        moves.push([x, left_side], [x, right_side])
+        moves.push([bottom_side, left_side],[bottom_side, y], [bottom_side, right_side])
+
+        moves
     end
 end
--- Ruby/object-oriented_programming/chess/knight.rb
+++ Ruby/object-oriented_programming/chess/knight.rb
@@ -1,12 +1,25 @@
 require_relative 'piece'
+require_relative 'module_stepable'
 
 class Knight < Piece
+    include Stepable
     def initialize(color, board, pos)
         super
     end
 
     def symbol
         :N
+    end
+
+    # return array of all possible moves without obstacles
+    def move_difs
+        x,y = self.pos[0], self.pos[1]
+        moves = []
+        moves.push([x-2, y-1], [x-2,y+1])
+        moves.push([x+2, y-1], [x+2, y+1])
+        moves.push([x-1, y-2], [x-1, y+2])
+        moves.push([x+1, y-2], [x+1, y+2])
+        moves
     end
 
 end
