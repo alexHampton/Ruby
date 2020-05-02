9928e51c48a2c71e8401a1a32d88b49d7b373291
Alex Hampton <ahampton5158@gmail.com>
2020-05-02 21:16:33 +0900

    Create NullPiece class, include debug history

---
+++ Ruby/object-oriented_programming/chess/null_piece.rb
@@ -0,0 +1,18 @@
+require_relative 'piece'
+require 'singleton'
+
+class NullPiece < Piece
+    include Singleton
+
+    attr_reader :color
+    def initialize
+        @color = nil
+    end
+
+    def symbol
+        :O
+    end
+
+end
+
+
