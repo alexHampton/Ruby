98745c046a95d87fce9f29e71cffb2cdcf777dfc
Alex Hampton <ahampton5158@gmail.com>
2020-05-02 18:27:48 +0900

    Create module_stepable for use by king and knight pieces

---
+++ Ruby/object-oriented_programming/chess/module_stepable.rb
@@ -0,0 +1,22 @@
+module Stepable
+
+    # takes move_difs from King/Knight and returns only the moves that are possible
+    def moves
+        moves = move_difs
+        # reject moves that would be off the board
+        moves.reject! {|move| move.any? {|num| num < 0 || num > 7 }}
+        # reject moves where allies stand
+        moves.reject! do |move|
+            if !self.board[*move].nil?
+                self.board[*move].color == self.color
+            end
+        end
+        moves
+
+    end
+
+    private
+    def move_difs
+        
+    end
+end
