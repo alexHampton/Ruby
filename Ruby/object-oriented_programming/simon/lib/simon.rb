24d2828e8de7084da0f0a53ee1628c38ef822c1c
Alex Hampton <ahampton5158@gmail.com>
2020-05-03 16:03:15 +0900

    Pass Rspec of Simon. Add some console functionality

--- Ruby/object-oriented_programming/simon/lib/simon.rb
+++ Ruby/object-oriented_programming/simon/lib/simon.rb
@@ -4,38 +4,74 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = Array.new
 
   end
 
   def play
-
+    until @game_over
+    take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !@game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      STDOUT.print "\r#{color}"
+      sleep 0.5
+      STDOUT.print "\r       "
+      sleep 0.5
+    end
+    puts
   end
 
   def require_sequence
+    i = 0
+    while i < @seq.length
+      input = gets.chomp
+      @game_over = input == @seq[i] ? false : true
+      break if @game_over
+      i += 1
+    end
 
   end
 
   def add_random_color
-
+    @seq << COLORS[rand(4)]
   end
 
   def round_success_message
-
+    puts "Good job!"
+    puts sequence_length.to_s
   end
 
   def game_over_message
+    puts "You lose."
+    puts "Your score was #{@sequence_length-1}."
 
   end
 
   def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = Array.new
 
   end
 end
+
+
+game = Simon.new
+game.play
