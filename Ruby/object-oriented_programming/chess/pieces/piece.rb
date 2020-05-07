79d03807ad7819ea087192745a16b87ac9828dd2
Alex Hampton <ahampton5158@gmail.com>
2020-05-08 07:56:25 +0900

    Add move_into_check method

--- Ruby/object-oriented_programming/chess/pieces/piece.rb
+++ Ruby/object-oriented_programming/chess/pieces/piece.rb
@@ -8,10 +8,19 @@ class Piece
     # this will be changed in each class for the pieces
     # should return an array of all possible moves
     def valid_moves
-        self.moves
+        moves.reject { |move| move_into_check?(move) }
     end
 
     def symbol
         :NA
     end
+
+    def move_into_check?(end_pos)
+        formatted_end_pos = end_pos.is_a?(String) ? @board.letter_to_pos(end_pos) : end_pos
+        potential_board = @board.dup
+        potential_board.move_piece!(self.pos, end_pos)
+        potential_board.in_check?(@color)
+    end
+
+    
 end
