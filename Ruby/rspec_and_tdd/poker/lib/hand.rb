c0671f511ada26461b629e2d5ae3a618054d2387
Alex Hampton <ahampton5158@gmail.com>
2020-05-17 13:48:14 +0900

    Add logic for hand rankings

--- Ruby/rspec_and_tdd/poker/lib/hand.rb
+++ Ruby/rspec_and_tdd/poker/lib/hand.rb
@@ -8,7 +8,7 @@ class Hand
     end
 
     def high_card
-        card_values.max
+        @values.max
     end
 
     def hand_ranking
@@ -21,7 +21,68 @@ class Hand
         return "Flush" if flush?
         return "Three of a Kind" if three_of_a_kind?
         return "Two Pair" if two_pair?
-        "No good"
+        return "One Pair" if one_pair?
+        "High Card"
+    end
+
+    # This will return an array of values of the hand rank.
+    # The first unequal value to be compared will determine the winner:
+    # Ex: [8, 4, 3, 0, 0] beats [7, 8, 9, 2, 3] because 14 is greater than 12
+    # It should return an array of 5 values becuase that's the maximum number of 
+    # values which would need to be compared in case, say, 2 players both have a
+    # straight, and they almost have the exact same numbers.
+    def hand_ranking_values
+        rank_name = hand_ranking
+        rank_values = []
+        case rank_name
+        when "Straight Flush"
+            rank_values.push(8, high_card)
+        when "Four of a Kind"
+            rank_values.push(7, n_of_a_kind(@values)[0])
+        when "Full House"
+            biggest_match = n_of_a_kind(@values)
+            triplet = biggest_match[0]
+            remaining_vals = @values.reject { |val| val == triplet}
+            doublet = n_of_a_kind(remaining_vals)[0]
+            rank_values.push(6, triplet, doublet)
+        when "Flush"
+            rank_values << 5
+            sorted = @values.sort
+            rank_values << sorted.pop until sorted.empty?
+        when "Straight"
+            rank_values << 4
+            sorted = @values.sort
+            if sorted.max == 14 && sorted[-2] == 5
+                rank_values << 5
+            else
+                rank_values << sorted.max
+            end
+        when "Three of a Kind"
+            triplet = n_of_a_kind(@values)[0]
+            remaining_vals = @values.reject { |val| val == triplet }
+            high, low = remaining_vals.max, remaining_vals.min
+            rank_values.push(3, triplet, high, low)
+        when "Two Pair"
+            big_match = n_of_a_kind(@values)[0]
+            remaining_vals = @values.reject { |val| val == big_match }
+            small_match = n_of_a_kind(remaining_vals)[0]
+            last_card = remaining_vals.reject { |val| val == small_match }[0]
+            rank_values.push(2, big_match, small_match, last_card)
+        when "One Pair"
+            match = n_of_a_kind(@values)[0]
+            remaining_vals = @values.reject { |val| val == match }
+            sorted_remaining_vals = remaining_vals.sort
+            rank_values.push(1, match)
+            rank_values.push(sorted_remaining_vals.pop) until sorted_remaining_vals.empty?
+        when "High Card"
+            sorted = @values.sort
+            rank_values << 0
+            rank_values.push(sorted.pop) until sorted.empty?
+        else
+            rank_values = ["This ain't right"]
+        end
+
+        rank_values
     end
 
     private
@@ -31,11 +92,10 @@ class Hand
     end
 
     def straight?
-        values = card_values
         best = high_card
         straight_builder = [best]
         (1..4).each do |n| 
-            break unless values.include?(best - n)
+            break unless @values.include?(best - n)
             straight_builder << (best - n)
         end
         return true if straight_builder.length == 5
@@ -44,7 +104,7 @@ class Hand
             least = 1
             straight_builder = [least]
             (1..4).each do |n|
-                break unless values.include?(least + n)
+                break unless @values.include?(least + n)
                 straight_builder << (least + n)
             end
             return true if straight_builder.length ==5
@@ -59,17 +119,24 @@ class Hand
 
     # returns an array of the values that make up the biggest match.
     def n_of_a_kind(vals)
-        same_vals = []
+        best_match = []
+        # iterate through each value
         vals.each_with_index do |val, idx|
             i = 0
             same = []
+            # for each value, check and see if there are other matches
             (idx..vals.length-1).each do |comp_idx|
                 i += 1 if vals[comp_idx] == val
+                # store those matches in 'same' array
                 same << vals[comp_idx] if vals[comp_idx] == val
             end
-            same_vals = same if same.length > same_vals.length
+
+            # update the best match if there are more of a kind, or if the match is of a bigger rank.
+            if (same.length > best_match.length) || (same.length == best_match.length && same[0] > best_match[0])
+                best_match = same
+            end
         end
-        same_vals
+        best_match
     end
 
     def four_of_a_kind?
@@ -91,21 +158,21 @@ class Hand
         removed_val = biggest_match[0]
         remaining_vals = @values.reject { |val| val == removed_val }
         return true if n_of_a_kind(remaining_vals).length == 2
-        # false
-        true
+        false
     end
 
+    # This can be used as is because it is only used after higher ranking 
+    # matches are checked. Ex: If this was used before full_house?, then full
+    # houses would be read as three of a kind.
     def three_of_a_kind?
         n_of_a_kind(@values).length == 3
     end
 
+    
     def one_pair?
-
+        n_of_a_kind(@values).length == 2
     end
 
-    def two_pair?
-
-    end
 
 
 end
--- Ruby/rspec_and_tdd/poker/spec/hand_spec.rb
+++ Ruby/rspec_and_tdd/poker/spec/hand_spec.rb
@@ -123,17 +123,153 @@ describe Hand do
             end
         end
 
-        # context "when best hand is a one pair" do
-        #     it "returns 'One Pair'"do
-        #         expect(hand.hand_ranking).to eq("One Pair")
-        #     end
-        # end
+        context "when best hand is a one pair" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "7H", :numerical_value => 7, :face_value => "H")}
+            let(:card4) {double("card", :value => "9S", :numerical_value => 9, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "returns 'One Pair'"do
+                expect(hand.hand_ranking).to eq("One Pair")
+            end
+        end
 
-        # context "when best hand is a high card" do
-        #     it "returns 'High Card'"do
-        #         expect(hand.hand_ranking).to eq("High Card")
-        #     end
-        # end
+        context "when best hand is a high card" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "TD", :numerical_value => 10, :face_value => "D")}
+            let(:card3) {double("card", :value => "7H", :numerical_value => 7, :face_value => "H")}
+            let(:card4) {double("card", :value => "9S", :numerical_value => 9, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "returns 'High Card'"do
+                expect(hand.hand_ranking).to eq("High Card")
+            end
+        end
         
+    end
+
+
+
+
+    describe "#hand_ranking_values" do
+        it "returns an array showing the ranking of the hand in integer form" do
+            expect(hand.hand_ranking_values.class).to eq(Array) 
+        end
+
+        context "when hand is a straight flush" do
+            it "inserts an 8 for the hand, plus a number for the high card value" do
+                expect(hand.hand_ranking_values).to eq([8, 13])
+            end
+        end
+
+        context "when hand is a four of a kind" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "6H", :numerical_value => 6, :face_value => "H")}
+            let(:card4) {double("card", :value => "6S", :numerical_value => 6, :face_value => "S")}
+            let(:card5) {double("card", :value => "AC", :numerical_value => 14, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts a 7 for the hand, plus a value for the matching number" do
+                expect(hand.hand_ranking_values).to eq([7, 6])
+            end
+        end
+
+        context "when hand is a full house" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "6H", :numerical_value => 6, :face_value => "H")}
+            let(:card4) {double("card", :value => "4S", :numerical_value => 4, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts a 6 for the hand, plus a value for the triplet,and for the pair" do
+                expect(hand.hand_ranking_values).to eq([6, 6, 4])
+            end
+        end
+
+        context "when hand is a flush" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "3C", :numerical_value => 3, :face_value => "C")}
+            let(:card3) {double("card", :value => "JC", :numerical_value => 11, :face_value => "C")}
+            let(:card4) {double("card", :value => "7C", :numerical_value => 7, :face_value => "C")}
+            let(:card5) {double("card", :value => "9C", :numerical_value => 9, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts a 5 for the hand, followed by a value for each card, high to low" do
+                expect(hand.hand_ranking_values).to eq([5, 11, 9, 7, 6, 3])
+            end
+        end
+
+        context "when hand is a straight" do
+            let(:card1) {double("card", :value => "KC", :numerical_value => 13, :face_value => "C")}
+            let(:card2) {double("card", :value => "QH", :numerical_value => 12, :face_value => "H")}
+            let(:card3) {double("card", :value => "JC", :numerical_value => 11, :face_value => "C")}
+            let(:card4) {double("card", :value => "TS", :numerical_value => 10, :face_value => "S")}
+            let(:card5) {double("card", :value => "AC", :numerical_value => 14, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts a 4 for the and, followed by the value of the highest card" do
+                expect(hand.hand_ranking_values).to eq([4, 14])
+            end
+        end
+
+        context "when hand is a low straight with an Ace" do
+            let(:card1) {double("card", :value => "5C", :numerical_value => 5, :face_value => "C")}
+            let(:card2) {double("card", :value => "4H", :numerical_value => 4, :face_value => "H")}
+            let(:card3) {double("card", :value => "3C", :numerical_value => 3, :face_value => "C")}
+            let(:card4) {double("card", :value => "2S", :numerical_value => 2, :face_value => "S")}
+            let(:card5) {double("card", :value => "AC", :numerical_value => 14, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+
+            it "sets 5 as the high card" do
+                expect(hand.hand_ranking_values).to eq([4, 5])
+            end
+        end
+
+        context "when hand is a 3 of a kind" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "6H", :numerical_value => 6, :face_value => "H")}
+            let(:card4) {double("card", :value => "7S", :numerical_value => 7, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts 3 for the hand, then the value of the triplet, and the ranks of the remaining cards" do
+                expect(hand.hand_ranking_values).to eq([3, 6, 7, 4]) 
+            end
+        end
+
+        context "when hand is a 2 pair" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "7H", :numerical_value => 7, :face_value => "H")}
+            let(:card4) {double("card", :value => "7S", :numerical_value => 7, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts 2 for the hand, then the rank of the high pair, the rank of the low pair, and the rank of the remaining card" do
+                expect(hand.hand_ranking_values).to eq([2, 7, 6, 4])
+            end
+        end
+
+        context "when hand is a 1 pair" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "6D", :numerical_value => 6, :face_value => "D")}
+            let(:card3) {double("card", :value => "7H", :numerical_value => 7, :face_value => "H")}
+            let(:card4) {double("card", :value => "9S", :numerical_value => 9, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts 1 for the hand, then the pair rank, and the ranks of the ramaining cards in descending order" do
+                expect(hand.hand_ranking_values).to eq([1, 6, 9, 7, 4]) 
+            end
+        end
+
+        context "when hand is a high card" do
+            let(:card1) {double("card", :value => "6C", :numerical_value => 6, :face_value => "C")}
+            let(:card2) {double("card", :value => "TD", :numerical_value => 10, :face_value => "D")}
+            let(:card3) {double("card", :value => "7H", :numerical_value => 7, :face_value => "H")}
+            let(:card4) {double("card", :value => "9S", :numerical_value => 9, :face_value => "S")}
+            let(:card5) {double("card", :value => "4C", :numerical_value => 4, :face_value => "C")}
+            let(:cards) { [card1, card2, card3, card4, card5] }
+            it "inserts a 0 for the hand, followed by the ranks of the cards in descending order" do
+                expect(hand.hand_ranking_values).to eq([0, 10, 9, 7, 6, 4]) 
+            end
+        end
     end
 end
